c682dec7cb6544ae6ced1a39a50df45a3d695038
Ben <meteoben@gmail.com>
2023-11-26 06:45:47 +0100

    passxage de tout le script en commentaire

--- js/script.js
+++ js/script.js
@@ -1,54 +1,54 @@
-// var sidenav = document.getElementById("mySidenav");
-// var openBtn = document.getElementById("openBtn");
-// var closeBtn = document.getElementById("closeBtn");
+// // var sidenav = document.getElementById("mySidenav");
+// // var openBtn = document.getElementById("openBtn");
+// // var closeBtn = document.getElementById("closeBtn");
 
-// openBtn.onclick = openNav;
-// closeBtn.onclick = closeNav;
+// // openBtn.onclick = openNav;
+// // closeBtn.onclick = closeNav;
 
-// /* Set the width of the side navigation to 250px */
-// function openNav() {
-//     sidenav.classList.add("active");
+// // /* Set the width of the side navigation to 250px */
+// // function openNav() {
+// //     sidenav.classList.add("active");
+// // }
+
+// // /* Set the width of the side navigation to 0 */
+// // function closeNav() {
+// //     sidenav.classList.remove("active");
+// // }
+
+
+
+
+
+// function myFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//         x.className += " responsive";
+//     } else {
+//         x.className = "topnav";
+//     }
 // }
 
-// /* Set the width of the side navigation to 0 */
-// function closeNav() {
-//     sidenav.classList.remove("active");
+
+
+// /* When the user clicks on the button,
+// toggle between hiding and showing the dropdown content */
+// function myFunction2() {
+//     document.getElementById("myDropdown").classList.toggle("show");
 // }
 
-
-
-
-
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
-
-
-
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction2() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
+// // Close the dropdown menu if the user clicks outside of it
+// window.onclick = function (event) {
+//     if (!event.target.matches('.dropbtn')) {
+//         var dropdowns = document.getElementsByClassName("dropdown-content");
+//         var i;
+//         for (i = 0; i < dropdowns.length; i++) {
+//             var openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains('show')) {
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
+//     }
+// }
 
 
 
